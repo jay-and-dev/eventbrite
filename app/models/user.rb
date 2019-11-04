f18d4591a28eb904beda0931ed001fdeb56872e7
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-11-04 23:06:17 +0100

    mail user development done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+
   has_many :events_hosted, foreign_key: 'admin_id', class_name: "Event"
   has_many :attendances
   has_many :events, through: :attendances
+  
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+  
 end
