8d0261d2657a19c72709618f7cedf42e188fb814
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-11-05 12:56:59 +0100

    index events done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@ require 'faker'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all
+#User.destroy_all
 Event.destroy_all
 Attendance.destroy_all
 
+#u = User.create(first_name: Faker::Name.name, last_name: Faker::Name.last_name, description: Faker::Movie.quote, email: "azoulco90@yopmail.com")
 
-1.times do |index|
-  u = User.create(first_name: Faker::Name.name, last_name: Faker::Name.last_name, description: Faker::Movie.quote, email: "azoulco" + index.to_s + "@yopmail.com")
-  e = Event.create(start_date: Date.today + 1, duration: 10 ,title: "Faker::Name.initials(number: 10)", description: "Faker::Name.initials(number: 30)", price: Faker::Number.between(from: 1, to: 1000), location: Faker::Nation.capital_city, admin_id: u.id)
-  a = Attendance.create(user_id: u.id, event_id: e.id)
+10.times do |index|
+  e = Event.create(start_date: Date.today + 1, duration: 10 ,title: "Event-#{index+1}", description: "lorem ipsum"*3, price: Faker::Number.between(from: 1, to: 1000), location: Faker::Nation.capital_city, admin_id: 19)
+  #a = Attendance.create(user_id: u.id, event_id: e.id)
 end
