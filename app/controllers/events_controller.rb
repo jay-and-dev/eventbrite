da69c6c42dbdbefe21fbaf9329a434f5ddc8db40
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-11-05 15:01:55 +0100

    navbar update with link for create new event done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,7 +16,7 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location], admin_id: current_user.id)
     if @event.save
-      flash[:success] = "Le potin a été créé"
+      #flash[:success] = "Le potin a été créé"
       redirect_to :show
     else
       render :new
