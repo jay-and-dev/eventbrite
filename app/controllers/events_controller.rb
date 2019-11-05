2aeba0fb55f26913185f0fed039aae4cda2789af
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-11-05 14:09:17 +0100

    updating the events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,33 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
-  
+  before_action :authenticate_user!, only: [:index, :new, :create]
+
   def index
     @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new 
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location], admin_id: current_user.id)
+    if @event.save
+      flash[:success] = "Le potin a été créé"
+      redirect_to :show
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    if @event.destroy
+      flash[:success] = "Le potin a été supprimé"
+      redirect_to action: 'index'
+    end
   end
 end
