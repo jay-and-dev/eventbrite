2e23aa2a25bde7492c48299727c2b820b85929b6
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-11-10 16:17:39 +0100

    created the admin namespace and the two controllers users and events

---
+++ app/controllers/admin/events_controller.rb
@@ -0,0 +1,13 @@
+class Admin::EventsController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'events/index'
+    get 'events/edit'
+    get 'events/update'
+    get 'events/destroy'
+  end
+  namespace :admin do
+    get 'users/index'
+    get 'users/destroy'
+  end
   devise_for :users
   
   root "events#index"
@@ -9,5 +19,10 @@ Rails.application.routes.draw do
 
   resources :events do
     resources :attendances
+  end
+
+  namespace :admin do
+    root "admin#index"
+    resources :users, :events
   end
 end
